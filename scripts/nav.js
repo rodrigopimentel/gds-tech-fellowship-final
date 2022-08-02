232872089689425164a11f31dd7cf6f47417f17d
rodrigopimentel <rodrigopimentel@uri.edu>
2022-08-02 17:31:11 -0400

    mobile navbar

---
+++ scripts/nav.js
@@ -0,0 +1,8 @@
+function showNav() {
+    var nav = document.getElementById('mobile-nav');
+    if (nav.style.display === 'block') {
+      nav.style.display = 'none';
+    } else {
+      nav.style.display = 'block';
+    }
+  }
